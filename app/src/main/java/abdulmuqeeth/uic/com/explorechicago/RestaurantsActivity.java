10b639304f73f020b3f9c2471a2b8329de0e86d6
abdulmuqeethmohammed <amoham62@uic.edu>
2018-03-31 22:45:49 -0500

    Implementing ListSelectionListener as an empty method

--- app/src/main/java/abdulmuqeeth/uic/com/explorechicago/RestaurantsActivity.java
+++ app/src/main/java/abdulmuqeeth/uic/com/explorechicago/RestaurantsActivity.java
@@ -2,8 +2,9 @@ package abdulmuqeeth.uic.com.explorechicago;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import abdulmuqeeth.uic.com.explorechicago.RestaurantNamesFragment.ListSelectionListener;
 
-public class RestaurantsActivity extends AppCompatActivity {
+public class RestaurantsActivity extends AppCompatActivity implements ListSelectionListener {
 
     static String[] restaurantTitles;
     @Override
@@ -12,5 +13,11 @@ public class RestaurantsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restaurants);
 
         restaurantTitles = getResources().getStringArray(R.array.restaurant_names);
+    }
+
+    //Implementing onListSelectionListener from interface ListSelectionListener of RestaurantNamesFragment
+    @Override
+    public void onListSelection(int index) {
+        //TODO
     }
 }
